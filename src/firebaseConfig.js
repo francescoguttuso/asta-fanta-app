d36e915da48d26e8688b293c8b792de896ae0e4a
francescoguttuso <francescoguttuso@gmail.com>
2026-08-06 08:10:22 +0200

    feat: update Firebase configuration to use environment variables for messagingSenderId and appId

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -10,8 +10,8 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "862180230703",
-  appId: "1:862180230703:web:db70c6fd3febb7a25d68da",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
